a589c87a58acfb7867946075f57a40185254dc44
MisterPrestoGamer <prestogamer12345@gmail.com>
2026-04-01 13:23:54 +0100

    basic structure and ideas

---
+++ models/createDb.js
@@ -0,0 +1,21 @@
+const { Client } = require('pg');
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, username VARCHAR ( 255 ), text VARCHAR ( 255 ), added VARCHAR ( 255 ));
+
+`;
+
+async function main() {
+  console.log('seeding...');
+  console.log(process.env.DATABASE_URL);
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log('done');
+}
+
+main();
